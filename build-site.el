49e209fdb74b099028293d1f508d200c3a9ec37d
KChousos <k.chousos@protonmail.com>
2022-07-27 01:55:47 +0300

    Simplified header until better solution, fixed footer

--- build-site.el
+++ build-site.el
@@ -12,7 +12,6 @@
 
 ;; Install dependencies
 (package-install 'htmlize)
-
 (setq org-html-htmlize-output-type 'css)
 
 ;; Load the publishing system
@@ -22,21 +21,28 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://gongzhitaao.org/orgcss/org.css\" />")
+      org-html-head
+      "<link rel=\"stylesheet\" href=\"https://gongzhitaao.org/orgcss/org.css\" />"
+      org-html-postamble
+      "<p>Written %C.</p>
+       <p>Made with <a href=\"https://www.gnu.org/software/emacs/\">Emacs</a>,
+                    <a href=\"https://orgmode.org/\">Org Mode</a> and
+                    <a href=\"https://github.com/gongzhitaao/orgcss\">Orgcss</a>.</p>")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
-       (list "org-site:main"
+       (list "blog:main"
              :recursive t
              :base-directory "./content"
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
-             :with-author nil           ;; Don't include author name
-             :with-creator t            ;; Include Emacs and Org versions in footer
+             ;; :with-author nil           ;; Don't include author name
+             ;; :with-creator nil          ;; Include Emacs and Org versions in footer
              :with-toc nil              ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file nil)))    ;; Don't include time stamp in file
+             ;; :time-stamp-file nil       ;; Don't include time stamp in file
+             )))
 
 ;; Generate the site output
 (org-publish-all t)
