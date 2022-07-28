63991b8789df2a9fdc4d7f54afa093abe55715e8
kchousos <79325777+kchousos@users.noreply.github.com>
2022-07-28 15:31:49 +0000

    Deploying to gh-pages from @ kchousos/kchousos.github.io@3edf819f8aa7c385e8000c9d7d1f12bf967bcd19 🚀

--- newtab.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-   $("a[href^='http']:not([href*='kchousos.github.io'])").each(function() {
-       $(this).click(function(event) {
-             event.preventDefault();
-             event.stopPropagation();
-             window.open(this.href, '_blank');
-        }).addClass('externalLink');
-   });
-});
