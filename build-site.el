1719a992de73de89d469a0bbe80c337d826e0162
KChousos <k.chousos@protonmail.com>
2022-07-31 15:36:51 +0300

    added highlight.js

--- build-site.el
+++ build-site.el
@@ -1,21 +1,16 @@
-;; Set the package installation directory so that packages aren't stored in the
-;; ~/.emacs.d/elpa path.
-(require 'package)
-(setq package-user-dir (expand-file-name "./.packages"))
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-;; Initialize the package system
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; Install dependencies
-(package-install 'htmlize)
-;; (setq org-html-htmlize-output-type 'css)
-
-;; Load the publishing system
 (require 'ox-publish)
+(require 'ox-html)
+
+;; ; Makes code blocks compatible with highlight.js
+(defun my/org-html-src-block (html)
+  "Modify the output of org-html-src-block for highlight.js."
+  (replace-regexp-in-string
+   "</pre>" "</code></pre>"
+   (replace-regexp-in-string
+    "<pre class=\"src src-\\(.*\\)\">"
+    "<pre><code class=\"\\1\">"
+    html)))
+(advice-add 'org-html-src-block :filter-return #'my/org-html-src-block)
 
 ;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
@@ -29,31 +24,25 @@
 
        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">
 
+       <link rel=\"stylesheet\" href=\"/js/highlight.js/styles/github.min.css\" media=\"screen\" />
+       <link rel=\"stylesheet\" href=\"/js/highlight.js/styles/github-dark.min.css\" media=\"screen and (prefers-color-scheme: dark)\" />
+
        <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"/icons/icon-light.png\" id=\"faviconTag\"/>
 
        <script type=\"text/javascript\">
             document.title += \" | Konstantinos Chousos\";
-       </script>
-       "
+       </script>"
+      
       org-html-preamble
       "<b>Konstantinos Chousos</b>
        <nav>
-       		<a href=\"/index.html\">
-                <i class=\"fa fa-home\"></i> Home
-            </a> |
-       		<a href=\"/articles.html\">
-            <i class=\"fa fa-book\"></i> Articles
-            </a> |
-       		<a href=\"/about.html\">
-                <i class=\"fa fa-user\"></i> About
-            </a> |
-       		<a href=\"https://github.com/kchousos\">
-                <i class=\"fa fa-github\"></i> Github
-            </a>
-       <nav>
-       "
+       		<a href=\"/\"> <i class=\"fa fa-home\"></i> Home </a> |
+       		<a href=\"/articles.html\"> <i class=\"fa fa-book\"></i> Articles </a> |
+       		<a href=\"/about.html\"> <i class=\"fa fa-user\"></i> About </a> |
+       		<a href=\"https://github.com/kchousos\"> <i class=\"fa fa-github\"></i> Github </a>
+       <nav>"
+
       org-html-postamble
-      ;; <p><a href=\"#top\">↑Top↑</a></p>
       "<hr>
        <center>
            <p>Made with <a href=\"https://www.gnu.org/software/emacs/\">Emacs</a>,
@@ -62,7 +51,10 @@
                         <br>Last edited: %C
            </p>
        </center>
+
        <script src=\"/js/favicon.js\"></script>
+       <script src=\"/js/highlight.js/highlight.min.js\"></script>
+       <script>hljs.highlightAll();</script>
        <script src=\"/js/external_links.js\"></script>")
 
 ;; Define the publishing project
@@ -73,6 +65,7 @@
              :base-directory "./content"
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
+             ;; :htmlized-source nil
              ;; :with-author nil           ;; Don't include author name
              ;; :with-creator nil          ;; Include Emacs and Org versions in footer
              :with-toc nil              ;; Include a table of contents
