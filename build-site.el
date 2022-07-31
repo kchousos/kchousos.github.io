8a1a489dd6b9546754bc7221c80a8ddb2f520869
KChousos <k.chousos@protonmail.com>
2022-07-31 21:08:47 +0300

    added htmlize, because it errors out without it

--- build-site.el
+++ build-site.el
@@ -1,4 +1,18 @@
-(setq org-html-htmlize-output-type nil)
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+(setq org-html-htmlize-output-type 'nil)
 
 (require 'ox-publish)
 (require 'ox-html)
