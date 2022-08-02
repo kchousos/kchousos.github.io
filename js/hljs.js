1396855cba954704bd5eb1d18ea946b5a759ecdb
kchousos <79325777+kchousos@users.noreply.github.com>
2022-08-02 18:06:28 +0000

    Deploying to gh-pages from @ kchousos/kchousos.github.io@0a6ac72bceacf294b4e2e42c99dcc31814b9a250 🚀

--- js/hljs.js
+++ js/hljs.js
@@ -1,4 +1,8 @@
 hljs.registerAliases("emacs-lisp", { languageName: 'lisp' }); 
 hljs.registerAliases("text", { languageName: 'plaintext' }); 
+hljs.registerAliases("undefined", { languageName: 'plaintext' });
 hljs.registerAliases("js", { languageName: 'javascript' });
+
+hljs.configure({ cssSelector: 'code' });;
+
 hljs.highlightAll();
