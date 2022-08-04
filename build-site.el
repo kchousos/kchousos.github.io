379723269dcc2bc7a6530dab0043729361727c17
KChousos <k.chousos@protonmail.com>
2022-08-04 17:24:29 +0300

    clickable-linkable headings

--- build-site.el
+++ build-site.el
@@ -27,6 +27,18 @@
 
 (advice-add 'org-html-src-block :filter-return #'my/org-html-src-block)
 
+;; From 'https://writepermission.com/org-blogging-clickable-headlines.html'
+(defun my-org-html-format-headline-function (todo todo-type priority text tags info)
+  "Format a headline with a link to itself."
+  (let* ((headline (get-text-property 0 :parent text))
+         (id (or (org-element-property :CUSTOM_ID headline)
+                 (org-export-get-reference headline info)
+                 (org-element-property :ID headline)))
+         (link (if id
+                   (format "<a href=\"#%s\">%s</a>" id text)
+                 text)))
+    (org-html-format-headline-default-function todo todo-type priority link tags info)))
+
 ;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
@@ -80,7 +92,8 @@
              :base-directory "./content"
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
-             ;; :htmlized-source nil
+             :html-format-headline-function 'my-org-html-format-headline-function
+            ;; :htmlized-source nil
              ;; :with-author nil           ;; Don't include author name
              ;; :with-creator nil          ;; Include Emacs and Org versions in footer
              :with-toc nil              ;; Include a table of contents
